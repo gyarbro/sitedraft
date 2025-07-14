d42046d30fe9939e41544bbc496d9656a86892a9
gyarbro <gyarbro@gmu.edu>
2025-07-13 23:23:24 -0400

    up

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -158,17 +158,3 @@
 
 })(jQuery);
 
-<script>
-document.addEventListener('DOMContentLoaded', function () {
-  const toggles = document.querySelectorAll('.dropdown-toggle');
-
-  toggles.forEach(toggle => {
-    toggle.addEventListener('click', () => {
-      const content = toggle.nextElementSibling;
-
-      // Toggle the "open" class
-      content.classList.toggle('open');
-    });
-  });
-});
-</script>
